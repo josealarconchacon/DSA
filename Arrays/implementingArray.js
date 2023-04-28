0208bd9e6577ade053606da0545979d7491e5ffd
josealarconchacon <jose.alarcon.chacon@gmail.com>
2023-04-27 20:47:52 -0400

    adding item to the end of array

--- Arrays/implementingArray.js
+++ Arrays/implementingArray.js
@@ -1,3 +1,22 @@
 "strict";
 
-class Colors {}
+class Color {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  // get data from memory
+  get(index) {
+    return this.data[index];
+  }
+  // push item to the end of the array
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+}
+
+const newColor = new Color();
+newColor.push("Red");
+console.log(newColor);
