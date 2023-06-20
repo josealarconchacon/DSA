295ec39b7b00f0cf8e8c4a0f73ef3c8ebadf19a9
josealarconchacon <jose.alarcon.chacon@gmail.com>
2023-06-19 21:56:49 -0400

    object

--- TS-Example/objects.ts
+++ TS-Example/objects.ts
@@ -2,13 +2,17 @@
 const account: {
   // annotating an object
   name: string;
+  age: number;
   balance: number;
+  city: string;
   // here, the status is missing from the object
   // we can get around this error, by making the status optional using ?
   status?: string;
 } = {
   name: "Marcus",
+  age: 89,
   balance: 10,
+  city: "NY",
 };
 
 // this syntax will store an array of objects
