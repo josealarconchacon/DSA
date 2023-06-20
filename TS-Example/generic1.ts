73592b77ae92bfb3a64ab9ace68bc9c312df8f3d
josealarconchacon <jose.alarcon.chacon@gmail.com>
2023-06-19 22:47:41 -0400

    generic

--- TS-Example/generic1.ts
+++ TS-Example/generic1.ts
@@ -7,3 +7,27 @@ function insertAtBeginning<T>(array: T[], value: T) {
 const myArray = [1, 2, 3];
 const updateArr = insertAtBeginning(myArray, -1); // -1,1,2,3
 const stArr = insertAtBeginning(["c", "f"], "f");
+
+// Class
+class Car {
+  name: string;
+  private model: string[];
+  cost: number;
+
+  constructor(name: string, model: string[], cost: number) {
+    this.name = name;
+    this.model = model;
+    this.cost = cost;
+  }
+
+  buy(newCar: string) {
+    this.model.push(newCar);
+  }
+  listOfCars() {
+    return this.model.slice();
+  }
+}
+
+const C1 = new Car("Honda", ["Model R", "Civic"], 20000);
+C1.buy("Civic");
+// C1.listOfCars()
