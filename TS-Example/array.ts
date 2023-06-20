4376fcaa3747aeab6d4d0ab7a1d7ad7aafa66480
josealarconchacon <jose.alarcon.chacon@gmail.com>
2023-06-19 21:52:32 -0400

    array

--- TS-Example/array.ts
+++ TS-Example/array.ts
@@ -2,10 +2,10 @@
 
 // the type of the array will be set to any
 let animals = [];
-// ts will assume the the array will be fill with string values
+// assume the the array will be fill with string values
 let animal: string[] = [];
 
-// to store an array with different types of values
+// array with different types of values
 // use the union type
 let numbers: number[] | string[] = [];
 
